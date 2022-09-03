b3d1e3899f0f14278778ef70175b4b4ab70d010b
jiaozihao <2812050796@qq.com>
2022-09-03 17:23:59 +0800

    123

--- src/main/java/com/ws/jzh/Test.java
+++ src/main/java/com/ws/jzh/Test.java
@@ -3,12 +3,8 @@ package com.ws.jzh;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
         System.out.println("hello git-demo");
-        System.out.println("hello git hot-fix");
-        System.out.println("asdasdasdasdasda");
+
+
     }
 }
